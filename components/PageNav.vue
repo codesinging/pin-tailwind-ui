391011b54ce0a309b9c457db12044f58ae86a2e4
codesinging <codesinging@gmail.com>
2021-08-04 22:43:50 +0800

    Add PageNav.vue

---
+++ components/PageNav.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="flex items-center justify-between h-12 mt-4 border-t">
+        <div class="h-full flex items-center">
+            <nuxt-link v-if="prev" :to="prev.to">
+                <icon-arrow-left-small/>
+                {{ prev.name }}
+            </nuxt-link>
+        </div>
+        <div class="h-full flex items-center">
+            <nuxt-link v-if="next" :to="next.to">
+                {{ next.name }}
+                <icon-arrow-right-small/>
+            </nuxt-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import menu from "../utils/memu";
+
+export default {
+    name: "PageNav",
+    computed: {
+        prev() {
+            return menu.prev(this.$route.path)
+        },
+        next() {
+            return menu.next(this.$route.path)
+        },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
